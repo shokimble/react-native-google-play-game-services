93edc2022f7f94cf3ffc9d18ccc91610c051db98
Federico Bricker <fbricker@dokkogroup.com.ar>
2019-05-19 01:17:36 -0300

    Fix promises returns on activityResult + report if the user denied or granted access to Saved Games

--- android/src/main/java/com/shokimble/rngoogleplaygameservices/RNGooglePlayGameServicesModule.java
+++ android/src/main/java/com/shokimble/rngoogleplaygameservices/RNGooglePlayGameServicesModule.java
@@ -22,6 +22,7 @@ import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 
 import android.util.Log;
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.annotation.NonNull;
@@ -76,6 +77,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
   private Promise signInPromise;
   private Promise achievementPromise;
   private Promise leaderboardPromise;
+  private Promise requestPermissionPromise;
+  private String requestPermissionFile;
   private Snapshot workingSnapshot;
 
   //activity result code
@@ -87,66 +90,87 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
   // tag for debug logging
   private static final String TAG = "shorngames";
 
+  /////////////////////////////////////////////////////////////////////////////
+
   private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-      super.onActivityResult(requestCode, resultCode, intent);
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+      super.onActivityResult(activity, requestCode, resultCode, intent);
 
-      if (requestCode == RC_ACHIEVEMENT_UI && achievementPromise != null)
+      if (requestCode == RC_ACHIEVEMENT_UI){
+        if(achievementPromise == null) return;
         achievementPromise.resolve("Achievement dialog complete");
+        return;
+      }
 
-      if (requestCode == RC_LEADERBOARD_UI && leaderboardPromise != null)
+      if (requestCode == RC_LEADERBOARD_UI){
+        if(leaderboardPromise == null) return;
         leaderboardPromise.resolve("Leaderboard dialog complete");
+        return;
+      }
+
+      if (requestCode == RC_REQUEST_PERMISSION_SUCCESS_CONTINUE_FILE_CREATION){
+        if(requestPermissionPromise == null) return;
+        if (resultCode == Activity.RESULT_OK) {
+          signInSilently(null);
+          requestPermissionPromise.reject("Drive.SCOPE_APPFOLDER: Granted");
+        }else{
+          requestPermissionPromise.reject("Drive.SCOPE_APPFOLDER: Denied");
+        }
+        requestPermissionFile = null;
+        requestPermissionPromise = null;
+        return;
+      }
 
       if (requestCode == RC_SIGN_IN) {
-        Log.d(TAG, "RC_SIGN_IN result returned");
         Task<GoogleSignInAccount> task =
                 GoogleSignIn.getSignedInAccountFromIntent(intent);
-
         try {
           GoogleSignInAccount account = task.getResult(ApiException.class);
           onConnected(account);
-          if (signInPromise != null)
+          if (signInPromise != null) {
             signInPromise.resolve("Signed in");
-
+          }
         } catch (ApiException apiException) {
           onDisconnected();
-          if (signInPromise != null)
+          if (signInPromise != null) {
             signInPromise.reject("Can't sign in");
+          }
         }
+        signInPromise = null;
+        return;
       }
     }
 
   };
 
+  /////////////////////////////////////////////////////////////////////////////
+
   public RNGooglePlayGameServicesModule (ReactApplicationContext reactContext)  {
     super(reactContext);
     this.reactContext = reactContext;
-
     reactContext.addActivityEventListener(mActivityEventListener);
-
-    //initialise client
-
-
-    //mGoogleSignInClient = GoogleSignIn.getClient(getCurrentActivity(),
-    //    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build());
-
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @Override
   public String getName() {
     return "RNGooglePlayGameServices";
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void isSignedIn(final Promise promise) {
-
     if(GoogleSignIn.getLastSignedInAccount(getCurrentActivity()) != null)
       promise.resolve("signed in");
     else
       promise.reject("not signed in");
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void signInSilently(final Promise promise) {
@@ -160,40 +184,27 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
                 if (task.isSuccessful()) {
                   Log.d(TAG, "signInSilently(): success");
                   onConnected(task.getResult());
-                  promise.resolve("silent sign in successful");
+                  if(promise!=null) promise.resolve("silent sign in successful");
                 } else {
                   Log.d(TAG, "signInSilently(): failure", task.getException());
                   onDisconnected();
-                  promise.reject("silent sign in failed");
+                  if(promise!=null) promise.reject("silent sign in failed");
                 }
               }
             }
     );
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void signInIntent(final Promise promise) {
     signInPromise = promise;
-
     GoogleSignInClient signInClient = GoogleSignIn.getClient(getCurrentActivity(), GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
-
     getCurrentActivity().startActivityForResult( signInClient.getSignInIntent(), RC_SIGN_IN);
-
-    /*
-    signInClient.getSignInIntent().addOnSuccessListener(new OnSuccessListener<Intent>() {
-      @Override
-      public void onSuccess(Intent intent) {
-        getCurrentActivity().startActivityForResult(intent, RC_SIGN_IN);
-      }
-    })
-    .addOnFailureListener(new OnFailureListener() {
-      @Override
-      public void onFailure(@NonNull Exception e) {
-        promise.reject("Could not launch sign in  intent");
-      }
-    });
-    */
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void signOut(final Promise promise) {
@@ -213,6 +224,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
             });
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void revealAchievement(String id, final Promise promise) {
     if(mAchievementsClient == null) {
@@ -222,6 +235,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     mAchievementsClient.reveal(id);
     promise.resolve("Revealed achievement");
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void unlockAchievement(String id, final Promise promise) {
@@ -233,6 +248,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     promise.resolve("Unlocked achievement");
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void incrementAchievement(String id, int inc, final Promise promise) {
     if(mAchievementsClient == null) {
@@ -243,6 +260,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     promise.resolve("Incremented achievement");
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void setAchievementSteps(String id, int steps, final Promise promise) {
     if(mAchievementsClient == null) {
@@ -252,6 +271,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     mAchievementsClient.setSteps(id, steps);
     promise.resolve("Achievement setps' set");
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void discardAndCloseSnapshot( final Promise promise ) {
@@ -267,6 +288,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     workingSnapshot = null;
     promise.resolve(null);
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void commitAndCloseSnapshot(String data, String description, final Promise promise ) {
@@ -291,13 +314,20 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     }
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void loadSnapshot(String name, final Promise promise ) {
+
     if(mSnapshotsClient == null) {
       promise.reject("Please sign in first");
       return;
     }
-    this.requestScopeAppFolder();
+
+    if (!GoogleSignIn.hasPermissions( googleSignInAccount, Drive.SCOPE_APPFOLDER)) {
+      this.requestScopeAppFolder(name, promise);
+      return;
+    }
 
     mSnapshotsClient.open(name, true, SnapshotsClient.RESOLUTION_POLICY_MANUAL)
       .addOnSuccessListener(new OnSuccessListener<DataOrConflict<Snapshot>>() {
@@ -340,6 +370,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
       });
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void setLeaderboardScore(String id, int score, final Promise promise) {
     if(mLeaderboardsClient == null) {
@@ -349,6 +381,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     mLeaderboardsClient.submitScore(id, score);
     promise.resolve("Leaderboard score set");
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void getLeaderboardScore(String id, final Promise promise) {
@@ -379,6 +413,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
       });
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void showAchievements(final Promise promise) {
     if(mAchievementsClient == null) {
@@ -402,6 +438,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
               }
             });
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   @ReactMethod
   public void showAllLeaderboards(final Promise promise) {
@@ -427,6 +465,8 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
             });
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+
   @ReactMethod
   public void showLeaderboard(String id, final Promise promise) {
     if(mLeaderboardsClient == null) {
@@ -451,15 +491,20 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
             });
   }
 
-  private void requestScopeAppFolder(){
-    if (!GoogleSignIn.hasPermissions( googleSignInAccount, Drive.SCOPE_APPFOLDER)) {
-      GoogleSignIn.requestPermissions(
-              getCurrentActivity(),
-              RC_REQUEST_PERMISSION_SUCCESS_CONTINUE_FILE_CREATION,
-              googleSignInAccount,
-              Drive.SCOPE_APPFOLDER);
-    }
+  /////////////////////////////////////////////////////////////////////////////
+
+  private void requestScopeAppFolder(String name, final Promise promise){
+    requestPermissionPromise = promise;
+    requestPermissionFile = name;
+    Log.d(TAG, "Drive.SCOPE_APPFOLDER will be requested");
+    GoogleSignIn.requestPermissions(
+      getCurrentActivity(),
+      RC_REQUEST_PERMISSION_SUCCESS_CONTINUE_FILE_CREATION,
+      googleSignInAccount,
+      Drive.SCOPE_APPFOLDER);
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   private void onConnected(GoogleSignInAccount googleSignInAccount) {
     Log.d(TAG, "onConnected(): connected to Google APIs");
@@ -476,7 +521,7 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     //mEventsClient = Games.getEventsClient(this, googleSignInAccount);
   }
 
-
+  /////////////////////////////////////////////////////////////////////////////
 
   private void onDisconnected() {
     Log.d(TAG, "onDisconnected()");
@@ -488,4 +533,7 @@ public class RNGooglePlayGameServicesModule extends ReactContextBaseJavaModule {
     mSnapshotsClient = null;
     //mEventsClient = null;
   }
+
+  /////////////////////////////////////////////////////////////////////////////
+
 }
